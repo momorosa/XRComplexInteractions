a1f74506beb042ed2559a04e410e3f9eebed85eb
momorosa <momorosa.design@gmail.com>
2023-08-25 15:39:40 -0700

    Add colliders and rigidbodies to hand avatars to increase the realism. Add hand script to the scene enabled phsics colliders on hand avatars.

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.InputSystem;
+
+public enum HandType
+{
+    Left,
+    Right
+};
+
+public class Hand : MonoBehaviour
+{
+
+public HandType type = HandType.Left;
+public bool isHidden {get; private set;} = false;
+public InputAction trackedAction = null;
+
+bool m_isCurrentlyTracked = false;
+
+List<MeshRenderer> m_currentRenderers = new List<MeshRenderer>();
+
+Collider[] m_colliders = null;
+
+public bool isCollisionEnabled { get; private set;} = false;
+
+    void Start()
+    {
+        m_colliders = GetComponentsInChildren<Collider>().Where(childCollider => !childCollider.isTrigger).ToArray();
+        trackedAction.Enable();
+        Hide();
+    }
+
+    void Update()
+    {
+        float isTracked = trackedAction.ReadValue<float>();
+        if (isTracked == 1.0f && !m_isCurrentlyTracked)
+        {
+            m_isCurrentlyTracked = true;
+            Show();
+        }
+        else if (isTracked == 0 && m_isCurrentlyTracked)
+        {
+            m_isCurrentlyTracked = false;
+            Hide();
+        }
+    }
+
+    public void Show()
+    {
+        foreach(MeshRenderer renderer in m_currentRenderers)
+        {
+            renderer.enabled = true;
+        }
+        isHidden = false;
+        EnableCollisions(true);
+    }
+
+    public void Hide()
+    {   
+        m_currentRenderers.Clear();
+        MeshRenderer[] renderers = GetComponentsInChildren<MeshRenderer>();
+        foreach(MeshRenderer renderer in renderers)
+        {
+            renderer.enabled = false;
+            m_currentRenderers.Add(renderer);
+        }
+        isHidden = true;
+        EnableCollisions(false);
+    }
+
+    public void EnableCollisions (bool enabled)
+    {
+        if (isCollisionEnabled == enabled) return;
+
+        isCollisionEnabled = enabled;
+        foreach (Collider collider in m_colliders)
+        {
+            collider.enabled = isCollisionEnabled;
+        }
+    }
+}
